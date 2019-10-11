cef49b969dea8648c897f29357e9fcf217d59f78
Kristin AB <misskristin12@hotmail.com>
2019-10-11 17:59:48 +0200

    Start of making "pre set" points

--- regular.cpp
+++ regular.cpp
@@ -27,15 +27,15 @@ void Regular::sort()
         std::swap(tempCoords[startIndex], tempCoords[smallestIndex]);
 
         if(tempCoords.at(startIndex).x < minX)
-        minX = tempCoords.at(startIndex).x;
+            minX = tempCoords.at(startIndex).x;
         else
             maxX = tempCoords.at(startIndex).x;
         if(tempCoords.at(startIndex).y < minY)
-        minY = tempCoords.at(startIndex).y;
+            minY = tempCoords.at(startIndex).y;
         else
             maxY = tempCoords.at(startIndex).y;
         if(tempCoords.at(startIndex).z < minZ)
-        minZ = tempCoords.at(startIndex).z;
+            minZ = tempCoords.at(startIndex).z;
         else
             maxZ = tempCoords.at(startIndex).z;
     }
@@ -44,17 +44,42 @@ void Regular::sort()
 bool Regular::compare(gsl::Vector3D a, gsl::Vector3D b)
 {
     double epsilon = 0.00001;
-    //sjekke om ax er mindre en bx
-    if(a.x < b.x ||(fabs (static_cast<double>(a.x-b.x)) < static_cast<double>(a.x)* epsilon && a.y <b.y))
+    //sjekke om ax er mindre en bx                                              //Prosjektet har y opp, så bruker z her
+    if(a.x < b.x ||(fabs (static_cast<double>(a.x-b.x)) < static_cast<double>(a.x)* epsilon && a.z <b.z))
         return true;
 
     return false;
 }
 
-void Regular::makeTriangles()
+void Regular::makeTriangles(int numInterval)
 {
-
+    //- Skal lage et rutenett basert på min og max verdier og delt opp i x interval.
+    //- Først sortere punktene.
     sort();
 
+    //- Dele opp i x antall deler (interval) mellom hvert punkt
+    calcLength();
+    intervalX = lengthX/numInterval;
+    intervalZ = lengthZ/numInterval;
+    // - lage standard punkter, som ikke er i noen triangler enda. Trianguleringen skjer etter dette
+    makeTempPoints();
+    // - finne nærmeste punkt til hvert egensatte punkt og bruke høyden til dette punktet
+    // - lage  triangler
 
+
+
+
+
+}
+
+void Regular::makeTempPoints()
+{
+
+}
+
+void Regular::calcLength()
+{
+    lengthX = maxX -minX;
+    lengthY = maxY -minY; //trenger vel egentlig ikke denne?
+    lengthZ = maxZ - minZ;
 }
